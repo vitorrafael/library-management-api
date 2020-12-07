86bcc12b87863d11a0ed95ba98532353ece9db26
vitorrafael <vrsilveira07@gmail.com>
2020-12-06 23:07:27 -0300

    Add new field and adjust comparing methods

--- src/main/java/com/vitorrafael/librarymanagement/models/Author.java
+++ src/main/java/com/vitorrafael/librarymanagement/models/Author.java
@@ -3,29 +3,36 @@ package com.vitorrafael.librarymanagement.models;
 import java.sql.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Author {
-	
+
 	@Id
-	private long id;
-	
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+
 	@NotEmpty
 	private String name;
-	
+
 	@NotEmpty
 	private String country;
-	
+
 	@NotEmpty
 	@DateTimeFormat
 	private Date birthday;
 
+	@Email
+	private String email;
+
 	public Author() {
-	
+
 	}
 
 	public Author(String name, String country, Date birthday) {
@@ -33,11 +40,42 @@ public class Author {
 		this.country = country;
 		this.birthday = birthday;
 	}
-
-	public long getId() {
-		return id;
+	
+	public Author(String name, String country, Date birthday, String email) {
+		this(name, country, birthday);
+		this.email = email;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -61,7 +99,9 @@ public class Author {
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
-	
-	
-	
+
+	public String getEmail() {
+		return email;
+	}
+
 }
